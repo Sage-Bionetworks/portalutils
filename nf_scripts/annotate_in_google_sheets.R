bc5084066e55ff86f9cc5ed91e94d6ef8a69f43a
Robert Allaway <robert@Roberts-Air.domain>
2019-09-19 10:41:07 -0700

    annotate in gs

---
+++ nf_scripts/annotate_in_google_sheets.R
@@ -0,0 +1,22 @@
+library(googlesheets)
+library(synapser)
+synLogin()
+gs_auth()
+
+##this script assumes you have settled your table's schema already
+
+tableId <- 'syn16858331'
+query <- "SELECT id, accessType, accessTeam FROM syn16858331 where benefactorId = 'syn20380009'"
+title <- sprintf("annotation_%s", Sys.time())
+
+tab <- synTableQuery(query)$asDataFrame()
+
+gs_new(title, input = tab)
+
+## go to google drive, open, make changes
+
+gs <- gs_title(title)
+tab_new <- gs_read(gs)
+
+table <- Table(tableId, tab_new)
+table <- synStore(table)
